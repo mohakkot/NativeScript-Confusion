2e7f6c13d41fb1454d0fee80cfe63dc01635ec7c
Mohak Kothari <mohak_kothari@hotmail.com>
2017-09-24 13:24:29 -0700

    Side Drawer

--- app/menu/menu.component.ts
+++ app/menu/menu.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit, Inject } from "@angular/core";
+import { Component, OnInit, Inject, ChangeDetectorRef } from "@angular/core";
 import { Dish } from "../Shared/Dish";
 import { DishService } from '../services/dish.service';
+
+import { DrawerPage } from '../shared/drawer/drawer.page';
 
 @Component({
     selector: 'app-menu',
@@ -9,11 +11,15 @@ import { DishService } from '../services/dish.service';
     styleUrls: ['./menu.component.css']
 })
 
-export class MenuComponent implements OnInit{
+export class MenuComponent extends DrawerPage implements OnInit{
     dishes: Dish[];
     errMess : string;
  
-    constructor(private dishService: DishService, @Inject('BaseURL') private BaseURL){ }
+    constructor(private dishService: DishService, 
+        private changeDetectorRef: ChangeDetectorRef,
+        @Inject('BaseURL') private BaseURL){ 
+            super(changeDetectorRef);
+        }
 
     ngOnInit() { 
         this.dishService.getDishes()
