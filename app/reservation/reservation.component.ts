b8f8e7ab93a694e78a7022012321dc2c40bb1250
Mohak Kothari <mohak_kothari@hotmail.com>
2017-10-07 22:08:47 -0700

    Assignment 3

--- app/reservation/reservation.component.ts
+++ app/reservation/reservation.component.ts
@@ -5,6 +5,13 @@ import { Validators, FormBuilder, FormGroup } from '@angular/forms';
 import { Switch } from 'ui/switch';
 import { ModalDialogService, ModalDialogOptions } from 'nativescript-angular/modal-dialog';
 import { ReservationModalComponent } from '../reservationmodal/reservationmodal.component';
+import { Page } from 'ui/page';
+import { View } from 'ui/core/view';
+import { SwipeGestureEventData, SwipeDirection } from 'ui/gestures';
+import { Animation, AnimationDefinition } from 'ui/animation';
+import * as enums from 'ui/enums';
+import { DatePipe } from "@angular/common";
+import { CouchbaseService } from '../services/couchbase.service';
 
 @Component({
     selector: 'app-reservation',
@@ -15,18 +22,37 @@ import { ReservationModalComponent } from '../reservationmodal/reservationmodal.
 export class ReservationComponent extends DrawerPage implements OnInit{
 
     reservation: FormGroup;
+    showForm: boolean=true;
+    gst: string;
+    smk: string="false";
+    dati : string;
+    formView: View;
+    dataView: View;
+    docId: string = "reservation";
+    reserve : Array<any>
 
     constructor(private changeDetectorRef: ChangeDetectorRef,
                 private formBuilder: FormBuilder,
                 private modalService : ModalDialogService,
+                private page: Page,
+                private couchbaseservice: CouchbaseService,
                 private vcRef: ViewContainerRef){ 
             super(changeDetectorRef);
+            this.reserve = [];
 
             this.reservation = this.formBuilder.group({
                 guests : 3,
                 smoking: false,
                 dateTime: ['', Validators.required]
             });
+
+            let doc = this.couchbaseservice.getDocument(this.docId);
+            if (doc==null){
+                this.couchbaseservice.createDocument({"reservation":[]}, this.docId);
+            }
+            else{
+                this.reserve = doc.reservation;
+            }
         }
     ngOnInit(){
 
@@ -35,25 +61,56 @@ export class ReservationComponent extends DrawerPage implements OnInit{
     onGuestChange(args){
         let textfield = <TextField>args.object;
         this.reservation.patchValue({ guests : textfield.text});
+        this.gst=textfield.text;       
     }
 
     onSmokingChecked(args){
         let smokingSwitch = <Switch>args.object;
         if(smokingSwitch.checked) {
             this.reservation.patchValue({smoking: true});
+            this.smk = 'true';
         }
         else{
             this.reservation.patchValue({smoking: false});
+            this.smk = 'false';
         }
     }
 
     onDateTimeChange(args){
         let textfield = <TextField>args.object;
         this.reservation.patchValue({dateTime: textfield.text});
+        this.dati = textfield.text;
     }
 
     onSubmit() {
-        console.log(JSON.stringify(this.reservation.value));
+        console.log(JSON.stringify(this.reservation.value));   
+        this.reserve.push(JSON.stringify(this.reservation.value));
+        this.couchbaseservice.updateDocument(this.docId, {"reservation":this.reserve});  
+        console.log(JSON.stringify(this.couchbaseservice.getDocument(this.docId)));
+        if (this.showForm ) {
+            this.animateOutIn();
+        }
+    }
+
+    animateOutIn(){
+        this.formView = this.page.getViewById<View>("formView");
+        this.dataView = this.page.getViewById<View>("dataView");  
+        this.formView.animate({
+            opacity: 0,
+            scale: { x:0, y:0},
+            duration: 500,
+            curve: enums.AnimationCurve.easeInOut
+        })
+        .then(()=> {
+            this.showForm=false;
+            this.dataView.animate({
+                opacity:1,
+                scale: {x:1, y:1},
+                duration:500,
+                curve: enums.AnimationCurve.easeInOut
+            });
+        });
+
     }
 
     createModalView(args){
--- app/shared/baseurl.ts
+++ app/shared/baseurl.ts
@@ -1,1 +1,2 @@
-export const baseURL = "http://192.168.0.14:3000/"
+//export const baseURL = "http://192.168.0.14:3000/"
+export const baseURL = "http://10.123.33.144:3000/"
